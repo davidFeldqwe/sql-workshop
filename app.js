d86813545ef05fea876495d1d01e216e962dca93
davidfeld <davidfeld@hilma.tech>
2025-11-24 09:37:32 +0200

    revert: views

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ var schoolRouter = require('./routes/school.routes');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
