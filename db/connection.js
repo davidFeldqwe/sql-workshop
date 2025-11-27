d567c2870ea7da1707cfa2efc6d32f491eb6dc35
davidfeld <davidfeld@hilma.tech>
2025-11-27 09:14:43 +0200

    revert

--- db/connection.js
+++ db/connection.js
@@ -1,21 +1,6 @@
 const mysql = require('mysql2');
 
-// Create MySQL connection
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'school_project'
-});
+//todo: Create MySQL connection
 
-// Connect to the database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL database:', err);
-    return;
-  }
-  console.log('Connected to MySQL database');
-});
-
-module.exports = connection;
+// module.exports = connection;
 
--- routes/school.routes.js
+++ routes/school.routes.js
@@ -17,34 +17,5 @@ router.get('/', (req, res) => {
   res.json({ message: 'TODO: Implement GET /schools endpoint' });
 });
 
-/**
- * POST /schools
- * Create a new school
- */
-router.post('/', (req, res) => {
-  // TODO: Extract school data from req.body
-  // TODO: Validate required fields
-  // TODO: Write SQL INSERT query
-  // TODO: Execute query using database connection
-  // TODO: Send response with created school data
-  // TODO: Handle errors appropriately
-  
-  res.json({ message: 'TODO: Implement POST /schools endpoint' });
-});
-
-/**
- * DELETE /schools/:id
- * Delete a school by ID
- */
-router.delete('/:id', (req, res) => {
-  // TODO: Extract school ID from req.params.id
-  // TODO: Write SQL DELETE query
-  // TODO: Execute query using database connection
-  // TODO: Send appropriate response (success or not found)
-  // TODO: Handle errors appropriately
-  
-  res.json({ message: 'TODO: Implement DELETE /schools/:id endpoint' });
-});
-
 module.exports = router;
 
